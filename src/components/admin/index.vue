3db776c4d33d7f1ef5dce3fa3004cebd575b6bf1
mike <verysimple@gmail.com>
2018-11-12 19:03:47 -0500

    Set accessKey from admin/index page if present in query

--- src/components/admin/index.vue
+++ src/components/admin/index.vue
@@ -37,7 +37,9 @@ export default {
         //let tenant = url.subdomain()
         const query = URI(window.location.search).query(true)
         let access_key = query.access_key
-        this.setAccessKey({accessKey:access_key})
+        if (access_key){
+            this.setAccessKey({accessKey:access_key})
+        }
 
         let lang = query.lang
 
