c3f58a9dcf58913649189206b64c8d04c624ccd3
mike <verysimple@gmail.com>
2019-04-13 10:27:38 +0700

    Integrating new vue-cli

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+module.exports = {
+    runtimeCompiler: true,
+    devServer: {
+        allowedHosts: ['.productlist.local']
+    },
+    /*
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.esm.js",
+            vuex$: "vuex/dist/vuex.esm.js",
+        }
+    },
+    */
+    configureWebpack: {
+        plugins: [
+            new BundleAnalyzerPlugin()
+        ]
+    }
+}
