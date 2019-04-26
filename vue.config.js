688f86bac145eeab0400eb9ed59ef62fde023600
mike <verysimple@gmail.com>
2019-04-26 20:56:05 +0700

    Minor config editing

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,10 @@ module.exports = {
     devServer: {
         allowedHosts: ['.productlist.local']
     },
-    /*
-    resolve: {
-        alias: {
-            vue$: "vue/dist/vue.esm.js",
-            vuex$: "vuex/dist/vuex.esm.js",
-        }
+    pages:{
+        index: 'src/main.js',
     },
+    /*
     configureWebpack: {
         plugins: [
             //new BundleAnalyzerPlugin()
